4ee5984f75b6b677b3f145744061c6b08d464f87
LevBord-wowie <bordlev7@gmail.com>
2025-11-12 08:35:27 +0100

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta charset='UTF-8'>
         <title>Logowanie</title>
 
-        <link href="cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -42,5 +42,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 </body>
 </html>
+
 
 
